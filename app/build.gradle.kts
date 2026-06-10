83d5b10ffa6f69685054e495c9a5d3f5ab219059
galDeri23 <Galderi1999@gmail.com>
2026-06-10 19:09:16 +0300

    feat: home screen design - shift card, job card, search, header and category assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
+    implementation("androidx.core:core:1.17.0")
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
@@ -45,5 +49,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.17.0")
+
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
 }
